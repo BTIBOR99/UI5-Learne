4e58994cb820ea7c740b8e1a8e4203c87fcf19a5
TIBOR BIRO <biro.tibor99@gmail.com>
2024-10-10 09:28:11 +0200

    second commit

--- webapp/controller/Details.controller.js
+++ webapp/controller/Details.controller.js
@@ -7,14 +7,14 @@ sap.ui.define([
     function (Controller) {
         "use strict";
 
-        return Controller.extend("learning.controller.View", {
+        return Controller.extend("learning.controller.Details", {
             onInit: function () {
                 var oModel = new sap.ui.model.odata.ODataModel("/sap/opu/odata/sap/ZWT_NAME_ODATA_SRV/");
    
-                 oModel.read("DataSet", {
+                 oModel.read("DetailsSet", {
                      success: function(oData) {
                           this.DataModel = new sap.ui.model.json.JSONModel(oData.results);
-                          this.getView().setModel(this.DataModel, "DataModel");
+                          this.getView().setModel(this.DataModel, "DetailsModel");
                      }.bind(this),
                      error : function (oError){}
              },

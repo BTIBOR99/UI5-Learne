ed8ad63537852fe1380642ab9ffd5d776e8efe48
TIBOR BIRO <biro.tibor99@gmail.com>
2024-10-16 15:51:06 +0200

    third commit

--- webapp/controller/Details.controller.js
+++ webapp/controller/Details.controller.js
@@ -9,17 +9,32 @@ sap.ui.define([
 
         return Controller.extend("learning.controller.Details", {
             onInit: function () {
+                this.oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+                this.oRouter.getRoute("Details").attachMatched(this.onRouteMatched, this); 
+            //    const oRouter = this.getOwnerComponent().getRouter();
+            //    oRouter.getRoute("details").attachPatternMatched(this.onObjectMatched, this);
+            },
+
+            onRouteMatched: function(oEvent){
                 var oModel = new sap.ui.model.odata.ODataModel("/sap/opu/odata/sap/ZWT_NAME_ODATA_SRV/");
-   
-                 oModel.read("DetailsSet", {
-                     success: function(oData) {
-                          this.DataModel = new sap.ui.model.json.JSONModel(oData.results);
-                          this.getView().setModel(this.DataModel, "DetailsModel");
-                     }.bind(this),
-                     error : function (oError){}
-             },
-                             
-               )
+
+                oModel.read("DetailsSet", {
+                    success: function(oData) {
+                         this.DetailsModel = new sap.ui.model.json.JSONModel(oData.results);
+                         this.getView().setModel(this.DetailsModel, "DetailsModel");
+                    }.bind(this),
+                    error : function (oError){}
+            })
             }
+
+            // onObjectMatched(oEvent) {
+              
+                // this.byId("rating").reset();
+                // this.getView().bindElement({
+                //     path: "/" + window.decodeURIComponent(oEvent.getParameter("arguments").invoicePath),
+                //     model: "invoice"
+                // });
+            // }
+
         });
     });
--- webapp/controller/View.controller.js
+++ webapp/controller/View.controller.js
@@ -10,16 +10,23 @@ sap.ui.define([
         return Controller.extend("learning.controller.View", {
             onInit: function () {
                 var oModel = new sap.ui.model.odata.ODataModel("/sap/opu/odata/sap/ZWT_NAME_ODATA_SRV/");
-   
+
                  oModel.read("DataSet", {
                      success: function(oData) {
                           this.DataModel = new sap.ui.model.json.JSONModel(oData.results);
                           this.getView().setModel(this.DataModel, "DataModel");
                      }.bind(this),
                      error : function (oError){}
-             },
-                             
-               )
-            }
+                })
+            },
+            onPress: function(oEvent) {
+                var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+                oRouter.navTo("Details");
+                // const oItem = oEvent.getSource();
+                // const oRouter = this.getOwnerComponent().getRouter();
+                // oRouter.navTo("details", {
+                //     invoicePath: window.encodeURIComponent(oItem.getBindingContext("invoice").getPath().substr(1))
+                // });
+               }          
         });
     });
